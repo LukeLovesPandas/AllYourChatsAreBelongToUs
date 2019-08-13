ad11a1568e6270e69c131d3ad3ae0dde5ad246ee
LukeLovesPandas <ldieter@gmail.com>
2019-08-13 01:31:15 -0400

    A bit more cleanup

--- Contracts/SlackIntegration/Profile.cs
+++ Contracts/SlackIntegration/Profile.cs
@@ -4,46 +4,46 @@ namespace AllYourChatsAreBelongToUs.Contracts.SlackIntegration {
     [DataContract]
     public class Profile {
 
-        [DataMember]
+        [DataMember(Name="avatar_hash")]
         public string AvatarHash { get; set; }
 
-        [DataMember]
+        [DataMember(Name="status_text")]
         public string StatusText { get; set; }
 
-        [DataMember]
+        [DataMember(Name="status_emoji")]
         public string StatusEmoji { get; set; }
 
-        [DataMember]
+        [DataMember(Name="real_name")]
         public string RealName { get; set; }
 
-        [DataMember]
+        [DataMember(Name="display_name")]
         public string DisplayName { get; set; }
 
-        [DataMember]
+        [DataMember(Name="real_name_normalized")]
         public string RealNameNormalized { get; set; }
 
-        [DataMember]
+        [DataMember(Name="display_name_normalized")]
         public string DisplayNameNormalized {get; set;}
 
-        [DataMember]
+        [DataMember(Name="email")]
         public string Email { get; set; }
 
-        [DataMember]
+        [DataMember(Name="image_original")]
         public string ImageOriginal { get;set; }
 
-        [DataMember]
+        [DataMember(Name="image_24")]
         public string Image24 { get; set; }
-        [DataMember]
+        [DataMember(Name="image_32")]
         public string Image32 { get; set; }
-        [DataMember]
+        [DataMember(Name="image_48")]
         public string Image48 { get; set; }
-        [DataMember]
+        [DataMember(Name="image_72")]
         public string Image72 { get; set; }
-        [DataMember]
+        [DataMember(Name="image_192")]
         public string Image192 { get; set; }
-        [DataMember]
+        [DataMember(Name="image_512")]
         public string Image512 { get; set; }
-        [DataMember]
+        [DataMember(Name="team")]
         public string Team { get; set; }
     }
 
--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -23,30 +23,5 @@ namespace AllYourChatsAreBelongToUs.Controllers
             var userInfo = await _slackClient.GetUserInfo(appId,userId);
             return userInfo;
         }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
